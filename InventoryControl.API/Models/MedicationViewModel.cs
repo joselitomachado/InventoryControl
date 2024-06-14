845e74c007d0e4c64586edb366adf3dfac6b52a2
Joselito Machado <paguelosantos@hotmail.com>
2024-06-14 01:32:06 -0300

    feat: modelo de visualização

---
+++ InventoryControl.API/Models/MedicationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryControl.API.Models;
+
+public class MedicationViewModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string ActiveIngredient { get; set; } = string.Empty;
+    public string Manufacturer { get; set; } = string.Empty;
+    public string BarCode { get; set; } = string.Empty;
+    public string InternalCode { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+}
