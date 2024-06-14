dd1c70ce629cf7a2d1963b73d47ac6768b0ef754
Joselito Machado <paguelosantos@hotmail.com>
2024-06-14 01:32:22 -0300

    feat: add entidade

---
+++ InventoryControl.API/Entities/Medication.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryControl.API.Entities;
+
+public class Medication
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ActiveIngredient { get; set; } = string.Empty;
+    public string Manufacturer { get; set; } = string.Empty;
+    public string BarCode { get; set; } = string.Empty;
+    public string InternalCode { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+}
