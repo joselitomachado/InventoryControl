43f73dbb0f43198df1a635049442eec8f0134ebc
Joselito Machado <paguelosantos@hotmail.com>
2024-06-14 01:31:12 -0300

    feat: add auto mapper

---
+++ InventoryControl.API/Mappers/MedicationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using InventoryControl.API.Entities;
+using InventoryControl.API.Models;
+
+namespace InventoryControl.API.Mappers;
+
+public class MedicationProfile : Profile
+{
+    public MedicationProfile()
+    {
+        CreateMap<Medication, MedicationViewModel>();
+
+        CreateMap<MedicationInputModel, Medication>();
+    }
+}
