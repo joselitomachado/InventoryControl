9c902eb6617bf3647ac4f1b54c41c8ebab716fe8
Joselito Machado <paguelosantos@hotmail.com>
2024-06-14 01:32:45 -0300

    feat: add program

--- InventoryControl.API/Program.cs
+++ InventoryControl.API/Program.cs
@@ -1,15 +1,48 @@
+using InventoryControl.API.Mappers;
+using InventoryControl.API.Persistence;
+using InventoryControl.API.Services.Interfaces;
+using InventoryControl.API.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "InventoryControl.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Joselito Machado",
+            Email = "opaguelosantos@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/joselitomachado/")
+        }
+    });
+
+    var xmlFile = "InventoryControl.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddScoped<IMedicationInterface, MedicationRepository>();
+
+builder.Services.AddAutoMapper(typeof(MedicationProfile).Assembly);
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddDbContext<InventoryControlDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("InventoryControlCs"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
