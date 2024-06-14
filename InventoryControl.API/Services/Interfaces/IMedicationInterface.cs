8b4c4814f194a20f4620b7ba60a44dd8f7f96b57
Joselito Machado <paguelosantos@hotmail.com>
2024-06-14 01:31:24 -0300

    feat: add interfaces

---
+++ InventoryControl.API/Services/Interfaces/IMedicationInterface.cs
@@ -0,0 +1,13 @@
+﻿using InventoryControl.API.Entities;
+using InventoryControl.API.Models;
+
+namespace InventoryControl.API.Services.Interfaces;
+
+public interface IMedicationInterface
+{
+    Task<List<MedicationViewModel>> GetAll();
+    Task<Medication> GetByBarcode(string barCode);
+    Task<Medication> Register(MedicationInputModel medication);
+    Task<Medication> Receipt(string barCode, int quantity);
+    Task<bool> Delete(Guid id);
+}
