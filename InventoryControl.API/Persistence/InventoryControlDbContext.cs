668d6088c5aa6647e04cf08de9122fa35fff15b1
Joselito Machado <paguelosantos@hotmail.com>
2024-06-14 01:30:57 -0300

    feat: add dbcontext

---
+++ InventoryControl.API/Persistence/InventoryControlDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryControl.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryControl.API.Persistence;
+
+public class InventoryControlDbContext : DbContext
+{
+    public InventoryControlDbContext(DbContextOptions<InventoryControlDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Medication> Medications { get; set; }
+}
